e915c8be408198874b0606a724a04bfe765fe5e9
Gilang Nova <133370197+ginov4110@users.noreply.github.com>
2023-10-02 20:27:52 +0700

    update main.jsx import typo

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 // import App from "./App.jsx";
-import App from "./routes/index"
-import Navbar from "./components/navbar";
+import App from "./routes/index";
+import Navbar from "./components/Navbar";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-	<React.StrictMode>
-		<Navbar/>
-		<App />
-	</React.StrictMode>
+  <React.StrictMode>
+    <Navbar />
+    <App />
+  </React.StrictMode>
 );
