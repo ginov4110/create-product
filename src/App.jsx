de51e6e4071efca1e36b4303eb69f1eac5a09e8c
Gilang Nova <133370197+ginov4110@users.noreply.github.com>
2023-10-02 20:30:55 +0700

    correction import

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import React from "react";
-import Navbar from "./components/navbar";
+import Navbar from "./components/Navbar";
 import { CreateProduct } from "./pages/create-product";
 import LandingPage from "./pages/landing-page";
 
 function App() {
-	return (
-		<>
-			<LandingPage/>
-			<CreateProduct />
-		</>
-	);
+  return (
+    <>
+      <LandingPage />
+      <CreateProduct />
+    </>
+  );
 }
 
 export default App;
